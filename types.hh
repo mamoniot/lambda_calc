fd6d288eb435d35d2ac4c07d16fb894a6f2af9f8
Monica <mmoniot2@gmail.com>
2019-11-12 22:54:21 -0800

    trial

--- types.hh
+++ types.hh
@@ -53,22 +53,86 @@ constexpr Uid FIRST_UID = 2;
 constexpr Uid NUM_UID_MASK = ~(1ll<<62);
 constexpr Uid VAR_UID_MASK = ~(1ll<<63);
 
-struct AST {
-	Part part;
-	union {
-		Uid var_uid;
-		struct {
-			AST* left;
-			AST* right;
-		} app;
-		struct {
-			Uid arg_uid;
-			AST* body;
-		} fn;
-	};
+
+struct AstLoc {
+	struct AstBuffer* buffer;
+	int32 i;
+};
+
+union AstData {
+	Uid var_uid;
+	struct {
+		AstBuffer* left_buffer;
+		AstBuffer* right_buffer;
+		int32 left_i;
+		int32 right_i;
+	} app;
+	struct {
+		Uid arg_uid;
+		AstLoc body;
+	} fn;
+	struct {
+		AstBuffer* next_buffer;
+		AstBuffer* prev_buffer;
+		int32 next_i;
+		int32 prev_i;
+	} free_list;
+};
+
+constexpr int32 AST_BUFFER_SIZE = KILOBYTE;
+struct AstBuffer {
+	AstBuffer* next;
+	AstBuffer* prev;
+	int32 size;
+	Part parts[AST_BUFFER_SIZE];
+	AstData data[AST_BUFFER_SIZE];
+};
+
+// struct Ast {
+// 	Part part;
+// 	union AstData {
+// 		Uid var_uid;
+// 		struct {
+// 			AST* left;
+// 			AST* right;
+// 		} app;
+// 		struct {
+// 			Uid arg_uid;
+// 			AST* body;
+// 		} fn;
+// 	};
+// };
+
+struct AstList {
+	AstBuffer* head;
+	AstLoc free_head;
 };
 
 struct VarLexer {
 	char** strings;
 	int32* sizes;
 };
+
+
+Part* get_part(AstLoc root) {
+	return &root.buffer->parts[root.i];
+}
+AstData* get_data(AstLoc root) {
+	return &root.buffer->data[root.i];
+}
+
+AstLoc reserve_term(AstList* ast_list) {
+	if(!ast_list->free_head.buffer) {
+		auto buffer = talloc(AstBuffer, 1);
+		if(ast_list->head) {
+
+		} else {
+			ast_list->head = buffer;
+			buffer->next = buffer;
+			buffer->prev = buffer;
+		}
+	}
+	auto new_term = ast_list->free_head;
+	ast_list->free_head = get_data(new_term)->next_free;
+	return new_term;
+}
